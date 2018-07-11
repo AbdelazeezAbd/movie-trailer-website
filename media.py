9df65a4a2450309df7f8cacd921ca8c7cf3fb6a1
Abdulaziz Abdulaziz <ajkar23@gmail.com>
2018-07-11 22:42:22 +0300

    Add the comments to the files

--- media.py
+++ media.py
@@ -1,10 +1,14 @@
 #defining the class is called movie
 class movie():
-    #defining __init__ function with the movie data parameters to take a place in the memory
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """here we put movies information that is required create
+       a movie trailer"""
+    #defining __init__ function with the movie data parameters
+    #to take a place in the memory
+    def __init__(self, movie_title, poster_image, trailer_youtube):
         #using self parameter we define instance variables
-        self.title = movie_title
-        self.storyline = movie_storyline
-        self.poster_image_url = poster_image
-        self.trailer_youtube_url = trailer_youtube
+        self.title = movie_title #defining an instance variable to put movie title
+        self.poster_image_url = poster_image #defining an instance variable
+                                             #to put poster image URL
+        self.trailer_youtube_url = trailer_youtube #defining an instance variable
+                                                   #to put trailer youtube URL
         #here is the end of instance variables
